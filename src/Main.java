422f8cbfc4ed1b97d6b1669da9ee7d32121cf989
kila1 <kila14081996@gmail.com>
2024-03-20 13:27:21 +0300

    Домашняя работа №10. Задание 3.

--- src/Main.java
+++ src/Main.java
@@ -4,15 +4,20 @@ public class Main {
         //Task 1
         System.out.println("Task 1");
 
-        String firstName = "Ivanov";
-        String middleName = "Ivan";
-        String lastName = "Ivanovich";
+        String firstName = "Иванов";
+        String middleName = "Семён";
+        String lastName = "Семёнович";
         String fullName = firstName +" "+ middleName +" "+ lastName;
         System.out.println("Ф. И. О. сотрудника — " + fullName);
 
         //Task 2
         System.out.println("Task 2");
         System.out.println("Данные Ф. И. О. сотрудника для заполнения отчета - " + fullName.toUpperCase());
+
+        //Task 3
+        System.out.println("Task 3");
+        String fullName1 = fullName.replace("ё","е");
+        System.out.println("Данные Ф. И. О. сотрудника - " + fullName1);
 
 
     }
