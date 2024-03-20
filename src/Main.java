d352fc997270f91257631734d32e87a2f30b625b
kila1 <kila14081996@gmail.com>
2024-03-20 13:08:57 +0300

    Домашняя работа №10. Задание 1.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello work 10!");
+
+        //Task 1
+        System.out.println("Task 1");
+
+        String firstName = "Ivanov";
+        String middleName = "Ivan";
+        String lastName = "Ivanovich";
+        String fullName = firstName +" "+ middleName +" "+ lastName;
+        System.out.println(fullName);
+
+
     }
 }
